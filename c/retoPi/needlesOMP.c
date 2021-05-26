c03759488e08abb03bb101e41fb4ef52daa82bd7
melissaeg <melissa.escobar@utp.edu.co>
2021-05-26 17:17:37 -0500

    Add OMP version of the algorithm

---
+++ c/retoPi/needlesOMP.c
@@ -0,0 +1,57 @@
+//to run, you must exec gcc needlesOMP.c -lm to avoid math lib problems
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#define numOfThreads 4 
+
+int calculatePartialPi(int size){
+    srand((unsigned int)time(NULL));
+    int acum = 0;    
+    #pragma omp parallel
+    for (int i=0;i<size;i++){
+        float distx = (float)rand()/RAND_MAX; // x distance
+        float rotation = (float)rand()/RAND_MAX;  // angle
+
+        float diff = distx - 0.75 * cosf(rotation);
+        if (diff <=0){
+            acum +=1;
+        }
+    }    
+    return acum;
+}
+
+float calculatePI(int size){
+    
+    int threads = 25; 
+    int arrayPartialSum[threads];
+    int asserts = 0;
+    int partialSize = size/ threads;
+
+    for(int i = 0; i < threads; i++){
+        int partialAsserts = calculatePartialPi(partialSize); 
+        asserts += partialAsserts;
+        arrayPartialSum[i]= partialAsserts;
+    }
+    float proportion = (float)asserts/ (float)size;
+    float calculatedPi = 2 / proportion;
+    
+    return calculatedPi;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int size = 10000000 ;    
+    clock_t start, end;
+    omp_set_num_threads(numOfThreads);
+
+    start = clock();
+    double calculatedPi = calculatePI(size);
+    end = clock();
+
+    double totalTime = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Valor calculado de pi %f en  %f  s", calculatedPi, totalTime); 
+    printf(" %f ", totalTime); 
+    return 0;    
+}
